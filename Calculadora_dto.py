f9c88798d7cdb794f2a4e5283563dbdd19f1b92d
Geborne <93268144+Geborne@users.noreply.github.com>
2022-02-21 23:37:03 -0300

    Add files via upload

---
+++ Calculadora_dto.py
@@ -0,0 +1,18 @@
+def soma(a, b):
+	return (a + b);
+
+def subtracao(a, b):
+	return (a - b);
+
+def multiplicacao(a, b):
+	return (a * b);
+
+def divisao(a, b):
+	return (a / b);
+
+num1 = float(input("Insira o primeiro numero: ")); num2 = float(input("Insira o segundo numero: "))
+
+soma(num1, num2)
+subtracao(num1, num2)
+multiplicacao(num1, num2)
+divisao(num1, num2)
